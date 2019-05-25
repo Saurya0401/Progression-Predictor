d286edb65a6f2adb9e837cd72c6680e88de58077
Sauryadeep Pal <sp04012000@gmail.com>
2019-05-25 22:11:52 +0800

    Update ProgressionPredictor.py
    - Added comments and docstrings.
    - Added input error handling to play_prog().

--- ProgressionPredictor.py
+++ ProgressionPredictor.py
@@ -5,10 +5,16 @@ try:
 except ModuleNotFoundError:
     pass
 
-# TODO: Handle input errors
+# TODO: Handle input errors in init_user().
 
 
 def format_inputs(inputs):
+    """
+    Formats inputs provided by init_user() then returns them to init_user().
+    :param inputs: raw inputs
+    :return: formatted inputs
+    """
+
     usertype = int(inputs[0])
     fname = str(inputs[1] + '.csv')
     start_chord_array = ProgPred.to_cnv([x.lower() for x in inputs[2].split(', ')])
@@ -20,12 +26,21 @@ def format_inputs(inputs):
 
 
 def create_datafile(filename):
+    """
+    Creates a (csv) file with a predetermined header.
+    """
+
     file = open(filename, 'w', newline='')
     file.writelines("C0,C1,C2,C3,L\n")
     file.close()
 
 
 def init_user():
+    """
+    Passes raw inputs from gui variables to format_inputs(), then uses the returned inputs to call functions that create
+    csv file.
+    """
+
     inputs = format_inputs([gui.usertype.get(), gui.username.get(), gui.start_chords.get(), gui.chords.get(),
                             gui.accuracy.get()])
     print(inputs)
@@ -37,11 +52,17 @@ def init_user():
 
 
 def test():
+    """
+    Tests a user provided chord progression.
+    :return: change state of gui.test_button to normal provided an instance of ProgTest is successfully created.
+    """
+
     filename = str(gui.username.get() + '.csv')
     test_list = ProgPred.to_cnv([x.lower() for x in gui.prog_input.get().split(', ')])
     print(test_list)
     try:
         if len(test_list[0]) == 4:
+            # create an instance of the ProgTest class provided no exceptions are raised.
             pred = ProgTest(df='users/{}'.format(filename), test_array=test_list[0])
             gui.output_textbox.config(text=pred.predict()[0])
             gui.info_textbox.config(text=pred.predict()[1])
@@ -61,16 +82,39 @@ def test():
 
 
 def play_prog():
+    """
+    Plays a user provided chord progression.
+    :return: change state of gui.test_button to normal provided an instance of ProgPred is successfully created.
+    """
+
     chords = ProgPred.to_cnv([x.lower() for x in gui.prog.get().split(', ')])
     print(chords)
-    prog = ProgPred()
-    prog.cnv = chords[0]
-    print(prog.cnv)
-    prog.play_prog()
-    return gui.play_button.config(state='normal')
+    try:
+        if len(chords[0]) == 4:
+            # create an instance of the ProgPred class provided no exceptions are raised.
+            prog = ProgPred()
+            prog.cnv = chords[0]
+            print(prog.cnv)
+            prog.play_prog()
+            return gui.play_button.config(state='normal')
+        elif len(chords[1]):
+            if chords[1][0] == '':
+                raise ValueError("Bad Input: No chords.")
+            else:
+                raise ValueError("Bad Input: Invalid chords: '{}'".format(", ".join(chords[1])))
+        else:
+            raise ValueError("Bad Input: Incorrect number of chords.")
+    except ValueError as input_error:
+        gui.error.config(text=input_error.args[0])
+        gui.show_error_window()
+        gui.play_button.config(state='normal')
 
 
 def main():
+    """
+    Creates a new user profile or calls the test() function on an existing user profile based on the user type.
+    """
+
     u_type = int(gui.usertype.get())
     f_name = str(gui.username.get() + '.csv')
     if u_type == 1:
